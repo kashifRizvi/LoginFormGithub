3d5e7b600ee86602c7f4f64405a1e7d830ef436d
mohdkashif289 <mohdkashif289@gmail.com>
2016-12-07 02:26:35 +0530

    Custom Cell and another view to show json

--- LoginForm/FormTableViewController.swift
+++ LoginForm/FormTableViewController.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
-class FormTableTableViewController: UITableViewController {
+class FormTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: Selector("keyboardAppeared:"), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
-        NotificationCenter.default.addObserver(self, selector: Selector("keyboardGetsHidden:"), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(FormTableTableViewController.keyboardAppeared), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(FormTableTableViewController.keyboardDisappeared), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
         
     }
     
     func keyboardAppeared(notification: Notification) {
+        print("Keyboard appeared!!")
         let userinfo = notification.userInfo
         let keyboardSize = (userinfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
         let edgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: (keyboardSize?.height)!, right: 0.0)
         tableView.contentInset = edgeInsets
         
         
+    }
+    
+    func keyboardDisappeared(notification: Notification){
+        
+        print("Keyboard Disappeared!!")
+
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -34,24 +41,63 @@ class FormTableTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
     
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let row = indexPath.row
+        var identifier = String()
+        
+        switch row {
+        case 0:
+            identifier = "cell0"
+        case 1:
+            identifier = "cell1"
+        case 2:
+            identifier = "cell2"
+        case 3:
+            identifier = "cell3"
+        case 4:
+            identifier = "cell4"
+        case 5:
+            identifier = "cell5"
+        case 6:
+            identifier = "cell6"
+        case 7:
+            identifier = "cell7"
+        case 8:
+            identifier = "cell8"
+        case 9:
+            identifier = "cell9"
+        default:
+            print("Can't Identify cells, Check reuse identifier.")
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
 
-        // Configure the cell...
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        if indexPath.row == 9 {
+            let user = User()
+            user.phoneNumber = 23425
+            print(user.phoneNumber)
+            print(user.firstName)
+            
+            
+            
+        }
+        
+    }
 
     /*
     // Override to support conditional editing of the table view.
---
+++ LoginForm/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  LoginForm
+//
+//  Created by Kashif Rizvi on 07/12/16.
+//  Copyright © 2016 Kashif. All rights reserved.
+//
+
+import Foundation
+
+class User {
+
+    var firstName : String?
+    var lastName : String?
+    var phoneNumber : Int64?
+    var email : String?
+    var password : String?
+    var birthDay : String?
+    
+    init(firstName:String?, lastName:String?, email:String?, password:String?, phoneNumber:Int64?, birthDay:String? ) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.password = password
+        self.birthDay = birthDay
+    }
+    
+    init() {
+    }
+    
+}
